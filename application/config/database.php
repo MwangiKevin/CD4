12f46f2cce32a73c3bced25f0e4e1ec86ee1fdcd
mwangikevin <mwangikevinn@gmail.com>
2014-03-27 16:46:04 +0300

    Server password sync

--- application/config/database.php
+++ application/config/database.php
@@ -50,7 +50,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = '';
+$db['default']['password'] = 'p@55w0rddv1';
 $db['default']['database'] = 'cd4tz';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
