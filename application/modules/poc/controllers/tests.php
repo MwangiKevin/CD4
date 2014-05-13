f4c4cde4277ff5ff147ce6464bc1ef1489ca4793
MwangiKevin <mwangikevinn@gmail.com>
2014-05-13 16:49:12 +0300

    modified POC test

--- application/modules/poc/controllers/tests.php
+++ application/modules/poc/controllers/tests.php
@@ -24,7 +24,7 @@ class tests extends MY_Controller {
 		$data['errors_agg'] = $this->poc_model->errors_reported();
 
 		$data['menus']	= 	$this->poc_model->menus(3);
-		$data['tests'] = 	$this->tests_model->get_tests_details($this->get_filter_start_date(),$this->get_filter_stop_date(),$this->session->userdata("user_filter_used"));
+		$data['tests'] = 	$this->tests_model->get_tests_details($this->get_filter_start_date(),$this->get_filter_stop_date(),$this->session->userdata("user_group_id"),$this->session->userdata("user_filter_used"));
 
 		$this -> template($data);
 	}
