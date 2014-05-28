50623bbedc3432f63a6839e0f2902f99ae545e61
bakasajoshua <baksajoshua09@gmail.com>
2014-05-28 11:21:26 +0300

    email functionality started

--- application/modules/utils/controllers/email.php
+++ application/modules/utils/controllers/email.php
@@ -10,16 +10,16 @@ class email extends MY_Controller {
 			'protocol' => 'smtp',
 			'smtp_host' => 'ssl://smtp.googlemail.com',
 			'smtp_port' => 465,
-			'smtp_user' => ,
-			'smtp_pass' => ,
+			'smtp_user' => 'cd4lims.tz@gmail.com',
+			'smtp_pass' => 'cd4lims.tz2014',
 			);
 
 		$this->load->library('email', $config);
 		$this->email->set_newline("\r\n");
 
-		$this->email->from()
+		$this->email->from('cd4lims.tz@gmail.com', 'CD4')
 		$this->email->to()
 		$this->email->subject()
-		$this->email->message('<html></html>');
+		$this->email->message();
 	}
 }
