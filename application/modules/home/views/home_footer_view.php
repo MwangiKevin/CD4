5847cdbc2abea48af664ce2d6f2fa5283e7e9392
MwangiKevin <mwangikevinn@gmail.com>
2014-08-19 12:07:29 +0300

    reset home dashboard

--- application/modules/home/views/home_footer_view.php
+++ application/modules/home/views/home_footer_view.php
@@ -107,14 +107,14 @@
 	}
 </script>
 
-<?php $this->equipment->equipment_pie(0,0); ?>
+<?php //$this->equipment->equipment_pie(0,0); ?>
 
-<?php $this->equipment->equipment_tests_pie(0,0); ?>
+<?php //$this->equipment->equipment_tests_pie(0,0); ?>
 
-<?php $this->tests->tests_pie(0,0); ?>
+<?php// $this->tests->tests_pie(0,0); ?>
 
-<?php $this->equipment->equipment_tests_column(0,0); ?>
+<?php //$this->equipment->equipment_tests_column(0,0); ?>
 
-<?php $this->tests->tests_line_trend(0,0); ?>
+<?php //$this->tests->tests_line_trend(0,0); ?>
 
-<?php $this->pima->expected_reporting_devices(0,0); ?>
+<?php //$this->pima->expected_reporting_devices(0,0); ?>
