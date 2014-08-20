1911278f3a36ff0e741646780209d853449fc3d4
MwangiKevin <mwangikevinn@gmail.com>
2014-08-20 13:34:31 +0300

    created procedures config
    Signed-off-by: MwangiKevin <mwangikevinn@gmail.com>

---
+++ application/config/procedures_sql.php
@@ -0,0 +1,21 @@
+<?php
+/*
+|--------------------------------------------------------------------------
+| PRESET CD4 SQL LIBRARY
+|--------------------------------------------------------------------------
+|
+| Path to the script directory.  Relative to the CI front controller.
+ * @package		sql
+ * @author		Kevin MWangi 
+ * @email 		mwangikevinn@gmail.com
+ * @usage 		-load config ->item("preset_sql");
+ *				-returns a predefines resultset
+ */
+$sql["v_facility_details"]  =	"
+
+								";
+
+$config["procedures_sql"] =$sql;
+
+/* End of file procedures_sql.php */
+/* Location: ./application/config/sql.php */
