41ffb445161f9fd0f95d662684f087acbfe75c44
MwangiKevin <mwangikevinn@gmail.com>
2014-08-21 12:18:28 +0300

    poc tests pie assync
    Signed-off-by: MwangiKevin <mwangikevinn@gmail.com>

--- application/modules/poc/views/home_footer_view.php
+++ application/modules/poc/views/home_footer_view.php
@@ -3,35 +3,13 @@
 $().ready(function() {
 
   $("#expected_reporting_devices").load("<?php echo base_url("charts/pima/expected_reporting_devices/$user_group_id/$user_filter_used"); ?>"); 
-  $("#expected_reporting_devices_pie").load("<?php echo base_url("charts/pima/expected_reporting_devices_pie/$user_group_id/$user_filter_used"); ?>"); 
+  $("#expected_reporting_devices_pie").load("<?php echo base_url("charts/pima/expected_reporting_devices_pie/$user_group_id/$user_filter_used"); ?>");
+  $("#testspie").load("<?php echo base_url("charts/tests/tests_pie/$user_group_id/$user_filter_used"); ?>"); 
   $("#pima-tests-table").load("<?php echo base_url("charts/pima/tests_table/$user_group_id/$user_filter_used"); ?>" ); 
 });
 
 
 </script>
-
-<script>
-
-
-
-
-
-
-$().ready(function() {
-      // expected_reporting_devices();
-      // expected_reporting_devices_pie();
-      // tests_pie();
-      // errors_pie();
-      // pima_error_types_col();
-      // pima_error_trend();
-    });
-
-
-</script>
-
-<?php //$this->pima->expected_reporting_devices($user_group_id,$user_filter_used); ?>
-
-<?php //$this->pima->expected_reporting_devices_pie($user_group_id,$user_filter_used); ?>
 
 <?php //$this->tests->tests_pie($user_group_id,$user_filter_used); ?>
 
