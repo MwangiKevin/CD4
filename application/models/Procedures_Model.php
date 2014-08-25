ccdf1af134f0fd0cfe1b8c7a192699b5ae1ae775
MwangiKevin <mwangikevinn@gmail.com>
2014-08-25 09:23:45 +0300

    A few more json files
    Signed-off-by: MwangiKevin <mwangikevinn@gmail.com>

--- application/models/Procedures_Model.php
+++ application/models/Procedures_Model.php
@@ -6,7 +6,13 @@ class Procedures_Model extends MY_Model{
 
 	}
 
-	public function get_facilities_details($user_group_id = 0,$user_filter_used = 0){
-		return R::getAll("call get_facilities_details('$user_group_id','$user_filter_used')");
+	public function get_facility_details($user_group_id = 0,$user_filter_used = 0){
+		return R::getAll("call get_facility_details('$user_group_id','$user_filter_used')");
+	}
+	public function get_region_details(){
+		return R::getAll("call get_region_details()");
+	}
+	public function get_district_details(){
+		return R::getAll("call get_district_details()");
 	}
 }
--- application/modules/utils/controllers/json.php
+++ application/modules/utils/controllers/json.php
@@ -27,7 +27,10 @@ class json extends MY_Controller {
 	* @job = to be run periodically and in case of certain change in lookup tables in the db
 	*/
 	public function write_all_json(){
+		
 		$this->write_facilities();
+		$this->write_regions();
+		$this->write_districts();
 	}
 
 
@@ -36,7 +39,7 @@ class json extends MY_Controller {
 	*/
 	public function write_facilities(){
 
-		$fac_assoc	=	$this->Procedures_Model->get_facilities_details(0,0);
+		$fac_assoc	=	$this->Procedures_Model->get_facility_details(0,0);
 		//print_r($fac_assoc);
 		//die();
 		foreach ($fac_assoc as $key => $value) {
@@ -50,6 +53,47 @@ class json extends MY_Controller {
 		file_put_contents($p, $facilities);
 
 		echo "Facilities Written <br/> ";
+
+	}
+
+	/**
+	* @job = writes regions info
+	*/
+	public function write_regions(){
+
+		$fac_assoc	=	$this->Procedures_Model->get_region_details();
+
+		foreach ($fac_assoc as $key => $value) {
+			$fac_assoc[$key]["value"] = $value["region_id"];
+			$fac_assoc[$key]["text"] = $value["region_name"];
+		}
+		$regions = 	json_encode($fac_assoc);
+
+		$p 	=	$this->path."regions.json";
+
+		file_put_contents($p, $regions);
+
+		echo "Regions Written <br/> ";
+
+	}
+	/**
+	* @job = writes districts info
+	*/
+	public function write_districts(){
+
+		$fac_assoc	=	$this->Procedures_Model->get_district_details();
+
+		foreach ($fac_assoc as $key => $value) {
+			$fac_assoc[$key]["value"] = $value["region_id"];
+			$fac_assoc[$key]["text"] = $value["region_name"];
+		}
+		$districts = 	json_encode($fac_assoc);
+
+		$p 	=	$this->path."districts.json";
+
+		file_put_contents($p, $districts);
+
+		echo "Districts Written <br/> ";
 
 	}
 
