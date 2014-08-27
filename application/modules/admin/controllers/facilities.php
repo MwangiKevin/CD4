2e74c6c20152cf4cf4a7b10f20cdda3582333b2a
MwangiKevin <mwangikevinn@gmail.com>
2014-08-27 15:00:51 +0300

    created ss function for admin facilities
    Signed-off-by: MwangiKevin <mwangikevinn@gmail.com>

--- application/modules/admin/controllers/facilities.php
+++ application/modules/admin/controllers/facilities.php
@@ -3,6 +3,12 @@ if (!defined('BASEPATH'))exit('No direct script access allowed');
 
 class facilities extends MY_Controller {
 
+	function __construct() {
+		parent::__construct();
+		
+		$this->load->model('admin_model');
+	}
+
 	public function index(){
 
 		$this->home_page();
@@ -16,7 +22,6 @@ class facilities extends MY_Controller {
 		$data['filter']	=	false;
 		$data	=array_merge($data,$this->load_libraries(array('admin_facilities')));
 		
-		$this->load->model('admin_model');
 
 		$data['menus']	= 	$this->admin_model->menus(2);
 
@@ -134,6 +139,70 @@ class facilities extends MY_Controller {
 		
 
 		
+	}
+
+	public function ss_dt_facilities(){
+		$facilities = 	$this->admin_model->db_filtered_view("v_facility_details",0);
+
+		$data 	=	array();
+		$recordsTotal =0;
+
+		foreach ($facilities as $key => $value) {
+
+
+				$facility_id			=	$value["facility_id"];
+				$facility_name			=	$value["facility_name"];
+				$facility_email			=	$value["facility_email"];
+				$facility_phone			=	$value["facility_phone"];
+				$facility_name			=	$value["facility_name"];
+				$district_name			=	$value["district_name"];
+				$region_name			=	$value["region_name"];
+				$partner_name			=	$value["partner_name"];
+				$partner_id				=	$value["partner_id"];
+				$equipment_count		=	$value["equipment_count"];
+				$users_count			=	$value["users_count"];
+				$facility_rollout_id	=	$value["facility_rollout_id"];
+				$facility_rollout_status	=	$value["facility_rollout_status"];
+
+			$color = "";
+			$class = "";
+
+			if($value['facility_rollout_id']==4){								
+				$color = "#2d6ca2";
+				$class = "glyphicon glyphicon-minus-sign";								
+			}elseif($value['facility_rollout_id']==1){							
+				$color = "#3e8f3e";
+				$class = "glyphicon glyphicon-ok-sign";								
+			}elseif($value['facility_rollout_id']==3){									
+				$color = "#c12e2a";
+				$class = "glyphicon glyphicon-remove-sign";							
+			}else{							
+				$color = "#eb9316";
+				$class = "glyphicon glyphicon-question-sign";														
+			}
+
+			$data[] = 	array(
+							($key+1),
+							$facility_name		,
+							$district_name		,
+							$region_name		,
+							$partner_name		,
+							$equipment_count	,
+							$users_count		,
+							"<center><a title='Active' href='javascript:void(null);'' style='border-radius:1px;' class='' onclick=\"edit_facility($facility_id,'$facility_name','$district_name','$region_name','$partner_id','$facility_email','$facility_phone',$facility_rollout_id)\"><span style='font-size: 1.4em;color: $color;' class='$class'></span></a><a></a></center>",							
+							"<center><a title='Edit Facility ($facility_name)' href='javascript:void(null);'' style='border-radius:1px;' class='' onclick=\"edit_facility($facility_id,'$facility_name','$district_name','$region_name','$partner_id','$facility_email','$facility_phone',$facility_rollout_id)\"><span style='font-size: 1.3em;color:#2aabd2;' class='glyphicon glyphicon-pencil'></span></a><a></a></center>",
+							
+						);
+			$recordsTotal++;
+		}
+		$json_req 	=	array(
+			"sEcho"						=> 1,
+			"iTotalRecords"				=>$recordsTotal,
+			"iTotalDisplayRecords"		=>$recordsTotal,
+			"aaData"					=>$data
+			);
+
+		echo json_encode($json_req);
 	}
 }
 /* End of file facilities.php */
