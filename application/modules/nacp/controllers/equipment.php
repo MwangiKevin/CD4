738bbda7238c9d1670841a946ab1ab334093176d
MwangiKevin <mwangikevinn@gmail.com>
2014-09-09 16:16:40 +0300

    made nacp equipment serverside
    Signed-off-by: MwangiKevin <mwangikevinn@gmail.com>

--- application/modules/nacp/controllers/equipment.php
+++ application/modules/nacp/controllers/equipment.php
@@ -2,6 +2,24 @@
 if (!defined('BASEPATH'))exit('No direct script access allowed');
 
 class equipment extends MY_Controller {
+	public $data = array();
+	public function __construct(){
+
+
+		$this->data['content_view'] = "nacp/nacp_equipment_view";
+		$this->data['title'] = "Equipment";
+		$this->data['sidebar']	= "nacp/nacp_sidebar_view";
+		$this->data['filter']	=	false;
+		$this->data	= array_merge($this->data,$this->load_libraries(array('dataTables','poc_equipment')));
+		
+		$this->load->model('nacp_model');
+
+		$this->data['devices_not_reported'] = $this->nacp_model->devices_not_reported();
+		$this->data['errors_agg'] = $this->nacp_model->errors_reported();
+
+		$this->data['menus']	= 	$this->nacp_model->menus(4);
+
+	}
 
 	public function index(){
 
@@ -10,23 +28,15 @@ class equipment extends MY_Controller {
 
 	public function home_page() {
 		$this->login_reroute(array(4));
-		$data['content_view'] = "nacp/nacp_equipment_view";
-		$data['title'] = "Equipment";
-		$data['sidebar']	= "nacp/nacp_sidebar_view";
-		$data['filter']	=	false;
-		$data	= array_merge($data,$this->load_libraries(array('dataTables','poc_equipment')));
-		
-		$this->load->model('nacp_model');
-
-		$data['devices_not_reported'] = $this->nacp_model->devices_not_reported();
-		$data['errors_agg'] = $this->nacp_model->errors_reported();
-
-		$data['menus']	= 	$this->nacp_model->menus(4);
 
 
-		$data['equipments'] = 	$this->nacp_model->get_details("equipment_details",$this->session->userdata("user_filter_used"));
+		$this->data['equipments'] = 	$this->nacp_model->get_details("equipment_details",$this->session->userdata("user_filter_used"));
 		//$data['equipments'] = 	$this->poc_model->equipments($this->session->userdata("user_filter_used"));
 		
-		$this -> template($data);
+		$this -> template($this->data);
+	}
+
+	public function test(){
+		$this -> template($this->data);
 	}
 }
