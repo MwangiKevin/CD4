ab283701e5c10979239925ba62459775fa324b1b
MwangiKevin <mwangikevinn@gmail.com>
2014-09-10 08:09:41 +0300

    designed tree
    Signed-off-by: MwangiKevin <mwangikevinn@gmail.com>

---
+++ application/modules/nacp/views/tree_view.php
@@ -0,0 +1,27 @@
+<div class = "row">
+    <div class="col-md-12" id ="desc">Hello</div>
+    <div class="col-md-3 tree well " style="overflow-y: auto; height: 300px !important;" id="tree">
+    </div>
+    <div class="col-md-9" style="overflow-y: auto; height: 300px !important;">
+    </div>
+</div>
+
+<script>
+
+$(function () {
+    //$("#tree").load("<?php echo base_url('nacp/tree/tree_schema')?>");
+
+    $.ajax({
+          type:"POST",
+          async:true,
+            url:"<?php echo base_url('nacp/tree/tree_schema')?>",  
+            success:function(data) {
+                $("#tree").html(data);
+                init_tree();
+            }
+      });
+
+
+});
+
+</script>
