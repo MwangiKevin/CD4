8743b45902ad352218572b8f4f8caeb04d3535c0
MwangiKevin <mwangikevinn@gmail.com>
2014-09-24 12:06:27 +0300

    completed map functionality
    Signed-off-by: MwangiKevin <mwangikevinn@gmail.com>

--- application/modules/charts/controllers/map.php
+++ application/modules/charts/controllers/map.php
@@ -4,10 +4,18 @@ if (!defined('BASEPATH'))exit('No direct script access allowed');
 class map extends MY_Controller {
 
 
+	function __construct() {
+		parent::__construct();
+
+		$this->load->model('map_model');
+	}
+
 
 	public function home() {
 
-		$this->load->view("home_map_view");
+		$data["map_data"] = $this->map_model->home_map_data();
+
+		$this->load->view("home_map_view",$data);
 	}
 }
 /* End of file charts.php */
