a67d7b44c40b0c448f89161ea777278afc3052aa
MwangiKevin <mwangikevinn@gmail.com>
2014-09-25 15:54:11 +0300

    cleaning code

--- application/modules/poc/models/poc_model.php
+++ application/modules/poc/models/poc_model.php
@@ -84,26 +84,6 @@ class poc_model extends MY_Model{
 
 	public function get_Upload_details($user_group_id,$user_filter_used){
 
-		// $user_delimiter =$this->sql_user_delimiter($user_group_id,$user_filter_used);
-
-		// $sql 	=	"SELECT 
-		// 					`pima_upload_id`,
-		// 					`upload_date`,
-		// 					`equipment_serial_number`,
-		// 					`facility_name`,
-		// 					`uploader_name`,
-		// 					COUNT(`pima_test_id`) AS `total_tests`,
-		// 					SUM(CASE WHEN `valid`= '1'    THEN 1 ELSE 0 END) AS `valid_tests`,
-		// 					SUM(CASE WHEN `valid`= '0'    THEN 1 ELSE 0 END) AS `errors`,
-		// 					SUM(CASE WHEN `valid`= '1'  AND  `cd4_count` < 350 THEN 1 ELSE 0 END) AS `failed`,
-		// 					SUM(CASE WHEN `valid`= '1'  AND  `cd4_count` >= 350 THEN 1 ELSE 0 END) AS `passed`
-		// 				FROM `v_pima_uploads_details`
-		// 				WHERE 1 
-		// 				$user_delimiter 
-		// 				GROUP BY `pima_upload_id`
-		// 				ORDER BY `upload_date` DESC
-		// 				LIMIT 500
-		// 			";
 
 
 		$sql 	=	"CALL get_uploads_dt($user_group_id,$user_filter_used)";
