81a2c38e48a754910551e1ad8f92256ba7c1ce10
MwangiKevin <mwangikevinn@gmail.com>
2014-09-26 12:21:26 +0300

    displaying the sw version at the footer

---
+++ application/modules/utils/controllers/versioning.php
@@ -0,0 +1,30 @@
+<?php
+if (!defined('BASEPATH'))exit('No direct script access allowed');
+
+class versioning extends MY_Controller {
+
+	function __construct() {
+		parent::__construct();
+	}
+	public function index(){
+
+		return $string = read_file('version.txt');
+	}
+	
+	public  function get_git_version() {
+		echo exec('git describe --exact-match');
+	}
+	public  function write_version() {
+		
+		$data = exec('git describe git describe --exact-match');
+
+		if ( ! write_file('version.txt', $data))
+		{
+			echo 'Unable to write the file';
+		}
+		else
+		{
+			echo 'File written!';
+		}
+	}	
+}
