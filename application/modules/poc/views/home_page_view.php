7a2bc01b3f28612f10f9b3923b453830845da9b2
MwangiKevin <mwangikevinn@gmail.com>
2014-10-03 16:00:24 +0300

    poc homepage ui

--- application/modules/poc/views/home_page_view.php
+++ application/modules/poc/views/home_page_view.php
@@ -1,4 +1,6 @@
 <div class="row">
+
+
 	<div class="tabbable span12">
 		<ul class="nav nav-tabs">
 			<!-- <li id ="tabSummary" ><a id = "linkSummary"  href="#tabs1-summary" data-toggle="tab">Summary</a></li> -->
@@ -9,37 +11,28 @@
 			<div class="tab-pane" id="tabs1-summary">
 			</div>
 			<!-- PIMA -->
-			<div class="tab-pane  active" id="tabs1-pima">
-				<div class="panel panel-default" style="float:left;width:100%;margin-top:5px;box-shadow: 4px 4px 4px #888888;">	
-					<div  id = "expected_reporting_devices" style = "float:left;width:60%;padding-right:20px;" >
-						<div class="loader" style"">Loading...</div>
-					</div>			
-					<div  id = "expected_reporting_devices_pie" style = "float:left;width:39%" >
-						<div class="loader" style"">Loading...</div>
-					</div>							
+			<div class="tab-pane  active row" id="tabs1-pima">
+				<div  id = "expected_reporting_devices" class="col-md-6">
+					<div class="loader" style"">Loading...</div>
+				</div>			
+				<div  id = "expected_reporting_devices_pie" class="col-md-3" >
+					<div class="loader" style"">Loading...</div>
+				</div>	
+				<div id = "testspie" class="col-md-3" >
+					<div class="loader" style"">Loading...</div>
+				</div>	
+				<div id="pima-tests-table" class="col-md-5" >							
+					<div class="loader" style"">Loading...</div>
 				</div>
-				<div class="panel panel-default" style="float:left;width:100%;box-shadow: 4px 4px 4px #888888;" >	
-					<div id = "testspie" style = "float:left;width:30%" >
-						<div class="loader" style"">Loading...</div>
-					</div>	
-					<div id = "errors_reported_pie" style = "float:left;width:30%" >
-						<div class="loader" style"">Loading...</div>
-					</div>
-					<div  style = "float:left;width:39%">
-						<div class="section-title" ><center> PIMA Tests for <?php echo $date_filter_desc;?> </center></div>
-						<div id="pima-tests-table" >							
-							<div class="loader" style"">Loading...</div>
-						</div>
-					</div>						
+				<div id = "errors_reported_pie" class="col-md-3" >
+					<div class="loader" style"">Loading...</div>
 				</div>
-				<div class="panel panel-default" style="float:left;width:100%;box-shadow: 4px 4px 4px #888888;">	
-					<div  id = "pima_error_types_col" style = "float:left;width:50%;padding:20px;" >
-						<div class="loader" style"">Loading...</div>
-					</div>			
-					<div  id = "pima_error_trend" style = "float:right;width:50%" >
-						<div class="loader" style"">Loading...</div>
-					</div>							
-				</div>
+				<div  id = "pima_error_types_col" class="col-md-3" >
+					<div class="loader" style"">Loading...</div>
+				</div>			
+				<div  id = "pima_error_trend" class="col-md-3" >
+					<div class="loader" style"">Loading...</div>
+				</div>	
 			</div>				
 		</div><!-- /.tab-content -->
 	</div><!-- /.tabbable -->
