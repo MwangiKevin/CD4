2212f911bc9bcf931852c840a1f1f02fe6115e8c
MwangiKevin <mwangikevinn@gmail.com>
2014-10-06 15:06:48 +0300

    removed fusion assets desc

--- application/config/asset.php
+++ application/config/asset.php
@@ -98,7 +98,6 @@ $config['plugin_js_files']	=	array(
 									array('title'	=> 'dhtmlxcommon'		,	'file'	=>	'dhtmlx/dhtmlxcommon.js'),
 									array('title'	=> 'dhtmlxcombo'		,	'file'	=>	'dhtmlx/dhtmlxcombo.js'),
 									array('title'	=> 'dhtmlxcombo_extra'	,	'file'	=>	'dhtmlx/dhtmlxcombo_extra.js'),
-									array('title'	=> 'FusionCharts'		,	'file'	=>	'Fusion/FusionCharts/FusionCharts.js'),
 									array('title'	=> 'jquery.fancybox.js?v=2.0.6'	,	'file'	=>	'FancyBox/jquery.fancybox.js?v=2.0.6'),
 									array('title'	=> 'fancybox'			,	'file'	=>	'FancyBox/jquery.fancybox.pack.js'),
 									array('title'	=> 'calendar'			,	'file'	=>	'Calendar/calendar.js'),
@@ -147,7 +146,6 @@ $config['plugin_css_files']	=	array(
 								);
 $config['plugin_php_files']	=	array(
 									array('title'	=> 	'phpexcel'		,			'file'	=>	'PHPExcel/PHPExcel.php'),
-									array('title'	=> 	'mpdf'			,			'file'	=>	'pdf/mpdf.php'),
-									array('title'	=>	'FusionCharts'	,			'file'	=>	'Fusion/FusionCharts/FusionCharts.php')
+									array('title'	=> 	'mpdf'			,			'file'	=>	'pdf/mpdf.php')
 								);
 /* End of file asset.php */
